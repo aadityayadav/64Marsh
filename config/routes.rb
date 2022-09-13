80c87eddaaf06ee3adb480ca2e0082077f066a3d
aadityayadav <67205038+aadityayadav@users.noreply.github.com>
2022-09-13 17:29:45 -0400

    static routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "static#index"
+  get "/home", to: "static#index"
+  get "/about", to: "static#about"
+  get "/contact", to: "static#contact"
 end
